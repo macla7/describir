cb18a0bdb0a29f6b03bc080d7d77c6df49b1ea30
Mitch <MitchelClark1997@outlook.com>
2024-07-19 16:58:30 +1000

    second part of maybe fix

--- auth.ts
+++ auth.ts
@@ -14,21 +14,41 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     Apple({
-      clientId: process.env.AUTH_APPLE_ID,
-      clientSecret: process.env.AUTH_APPLE_SECRET as string,
+      clientId: process.env.APPLE_ID,
+      clientSecret: process.env.APPLE_SECRET as string,
+      wellKnown: 'https://appleid.apple.com/.well-known/openid-configuration',
+      checks: ['pkce'],
       authorization: {
         url: 'https://appleid.apple.com/auth/authorize',
         params: {
-          scope: 'openid email name'
+          scope: 'name email',
+          response_type: 'code',
+          response_mode: 'form_post',
+          state: crypto.randomUUID()
         }
       },
+      token: {
+        url: `https://appleid.apple.com/auth/token`
+      },
+      client: {
+        token_endpoint_auth_method: 'client_secret_post'
+      },
       profile(profile) {
-        // Extract user information from the ID token
         return {
           id: profile.sub,
-          name: profile.name || profile.email.split('@')[0],
-          email: profile.email
+          name: profile.name || null,
+          email: profile.email || null,
+          image: null
         }
+      },
+      profileConform(profile, query) {
+        if (query.user) {
+          const user = JSON.parse(query.user)
+          if (user.name) {
+            profile.name = Object.values(user.name).join(' ')
+          }
+        }
+        return profile
       }
     }),
     Facebook,
